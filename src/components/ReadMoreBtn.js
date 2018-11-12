c54b4a69a97df1f88da03fdab71f51f43f12ec1a
vaidotasp <vaidotasp@gmail.com>
2018-11-11 19:44:50 -0500

    started work in info section

--- src/components/ReadMoreBtn.js
+++ src/components/ReadMoreBtn.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { Link } from 'gatsby';
+
 import styles from './components.module.css';
 
-const ReadMoreBtn = props => (
-  <button className={styles.readMoreBtn}>Skaityti Daugiau</button>
+const ReadMoreBtn = () => (
+  <Link to="/info">
+    <button className={styles.readMoreBtn}>Skaityti Daugiau</button>
+  </Link>
 );
 export default ReadMoreBtn;
--- src/pages/info.js
+++ src/pages/info.js
@@ -1,11 +1,90 @@
 import React from 'react';
 import Layout from '../components/layout';
+import styles from './info.module.css';
 import FloatingBtn from '../components/FloatingBtn';
-
+import OrderBtn from '../components/OrderBtn';
 const InfoPage = () => (
   <Layout>
-    <h1>InfoPage</h1>
-    <p>Find Infor Here</p>
+    <div className={styles.generalWrap}>
+      <h1 style={{ textAlign: 'center' }}>
+        Produkcijos informacija ir naudojimas
+      </h1>
+      <div className={styles.generalInfo}>
+        <div className={styles.generalDescription}>
+          <ul>
+            <li>
+              <h4>Aprašymas</h4>
+              <p>
+                Doktor Animals - tai naudingų bakterijų kultūrų ir augalinių
+                ekstraktų mišinys.
+              </p>
+            </li>
+            <li>
+              <h4>Sudėtis</h4>
+              <p>
+                Švarus nechloruotas vanduo, cukranendrių melasa, pieno rūgšties
+                bakterijos, mielės, natūrali acto rūgštis, etanolis, augalų
+                ekstraktai.
+              </p>
+            </li>
+            <li>
+              <h4>Kultūros</h4>
+              <p>
+                Lactobacillus casei (ATTC PTA 6135), Enterococcus faecium (NCIMB
+                10415, DSM 10663), Bacillus subtilis (C-3102, DSM 15544).
+              </p>
+            </li>
+            <li>
+              <h4>Ekologija ir GMO</h4>
+              <p>
+                Produktas yra nekenksmingas aplinkai ir nėra genetiškai
+                modifikuotas. 100% ekologinis produktas.
+              </p>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.imgPlaceholder}>Product Pic</div>
+      </div>
+      <div className={styles.animalDescWrap}>
+        <div className={styles.animalDesc}>
+          <div className={styles.animalDetail}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis
+            rerum quo fugit quaerat et, nisi ipsum deserunt tempora alias, minus
+            doloribus? Eligendi, commodi eos? Eum itaque quos distinctio
+            delectus modi.
+          </div>
+          <div className={styles.imgPlaceholder}>pic</div>
+        </div>
+        <div className={styles.animalDesc}>
+          <div className={styles.animalDetail}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis
+            rerum quo fugit quaerat et, nisi ipsum deserunt tempora alias, minus
+            doloribus? Eligendi, commodi eos? Eum itaque quos distinctio
+            delectus modi.
+          </div>
+          <div className={styles.imgPlaceholder}>pic</div>
+        </div>
+        <div className={styles.animalDesc}>
+          <div className={styles.animalDetail}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis
+            rerum quo fugit quaerat et, nisi ipsum deserunt tempora alias, minus
+            doloribus? Eligendi, commodi eos? Eum itaque quos distinctio
+            delectus modi.
+          </div>
+          <div className={styles.imgPlaceholder}>pic</div>
+        </div>
+        <div className={styles.animalDesc}>
+          <div className={styles.animalDetail}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis
+            rerum quo fugit quaerat et, nisi ipsum deserunt tempora alias, minus
+            doloribus? Eligendi, commodi eos? Eum itaque quos distinctio
+            delectus modi.
+          </div>
+          <div className={styles.imgPlaceholder}>pic</div>
+        </div>
+      </div>
+      <OrderBtn />
+    </div>
     <FloatingBtn />
   </Layout>
 );
